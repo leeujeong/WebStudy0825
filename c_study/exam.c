31725a64b59423981270371199f7a1e5eca484ea
이이유정 <ujeong@iiyujeong-ui-MacBookPro.local>
2021-09-29 00:14:46 +0900

    commit

---
+++ c_study/exam.c
@@ -0,0 +1,72 @@
+//1. 도서정보 저장 구조체
+typedef struct book{
+    int  bNo;
+    char bName[31];
+    char author[31];
+}Book;
+
+//2. 도서정보 구조체 배열 길이를 매크로 상수이용하여 10 으로 선언
+#define LENGTH 10
+
+//3. 매크로 상수 이용해 도서정보 구조체 배열을 전역 변수로 선언하고,
+//   배열의 인덱스역시 전역변수로 선언하여 모든함수에서 사용하도록 처리
+Book books[LENGTH];
+int i;
+
+//4. 메뉴 출력
+void menu(){
+printf(":::도서등록 프로그램:::");
+printf("1. 도서정보 등록");
+printf("2. 도서정보 조회");
+printf("3. 전체도서 목록");
+printf("0. 프로그램 종료");
+}
+
+//5. 새로운 도서정보를 입력받아서 도서정보 구조체 배열에 저장하는 함수를 구현
+//   새로운 도서정보는 항상 배열의 마지막 요소 다음위치에 저장하고, 배열이 가득찬 경우 저장할수 없다는 적절한 메세지 출력
+void addBook(){
+    for(i = 0; i<LENGTH; i++){
+        printf("신규 책 번호 입력 >>>"); scanf("%d", &books[i].bNo);
+        printf("신규 책 제목 입력 >>>"); scanf("%s", books[i].bName);
+        printf("신규 책 저자 입력 >>>"); scanf("%s", books[i].author);
+    }
+}
+
+
+//6. 도서번호를 입력받아서 해당도서의 제목과 저자를 출력하는 함수구현
+void queryBook(){
+   int num_book;
+    printf("조회할 책 번호 입력 >>>"); scanf("%d",&num_book);
+    for(int i =0; i<LENGTH; i++){
+    if(num_book == books[i].bNo){
+    printf("조회결과 : %s %s", books[i].bName, books[i].author);
+      }
+     }
+    }
+
+
+
+//7. 모든 도서의 도서번호, 제목, 저자정보를 조회하는 함수구현
+void printBook(){
+    for(i=0; i<LENGTH;i++){
+        printf("도서번호 : %d, 제목 : %s, 저자정보 : %s", books[i].bNo, books[i].bName, books[i].author);
+    }
+
+}
+//8. 지금까지 구현된 모든함수를 호출할수 있는 main 함수구현
+int main(){
+    int sel , totalSCmt=0;
+    while(1){
+        menu();
+        printf("메뉴선택(1,2,3,0 >>>") ; scanf("%d",&sel);
+        switch (sel)
+        {
+        case 1 : addBook(); break;
+        case 2 : queryBook(); break;
+        case 3 : printBook(); break;
+        case 0 : return 0;
+        default: puts("0~3 사이의 메뉴 번호를 선택하세요"); break;
+        }
+    }
+
+}
---
+++ c_study/exam2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+//1. 학생 정보를 저장할 구조체 작성
+typedef struct student{
+    char name[31];
+    int kor, eng, math;
+    int sum;
+    double avg;
+}Stuendt;
+
+//2. 학생정보 구조체 배열의 길이를 매크로 상수를 이용하여 3으로 선언
+#define LENGTH 3
+
+//3. 매크로 상수를 이용해 도서정보 구조체 배열을 전역변수로 선언하여 모든 함수에서 사용할 수 있도록 처리
+Stuendt students[LENGTH];
+
+//4. 새로운 학생정보를 입력받아서 학생정보 구조체 배열에 저장하는 함수구현
+//  이름은 공백입력 가능하도록 처리, 국어 영어 수학 점수만 입력하면 총점과 평균은 자동으로 계산된뒤 저장되도록 처리
+void inputinfomation(){
+    int i;
+    for(i =0; i<LENGTH; i++){
+    printf("학생 %d 이름 >>>", i+1); gets(students[i].name);
+    printf("국어 >>>"); scanf("%d", &students[i].kor);
+    printf("영어 >>>"); scanf("%d", &students[i].eng);
+    printf("수학 >>>"); scanf("%d", &students[i].math);
+    students[i].sum = students[i].kor +students[i].eng +students[i].math;
+    students[i].avg = students[i].sum / 3;
+    }
+    
+
+}
+//5. 학생정보구조체 배열에 저장된 모든 학생정보를 파일에 저장하는함수를 구현하시어
+//  파일명은 score.csv , "이름, 국어, 영어, 수학, 총점, 평균" 과 같은 형식으로 저장
+void generateFile(){
+    FILE * f = fopen("score.csv","wt");
+    if(f ==NULL){
+        fprintf(stderr, "file open error\n");
+        return;
+    }
+    int i;
+    for(int i=0; i<LENGTH; i++){
+    fprintf(f,"이름 : %s, 국어 : %d점\n, 영어 : %d점\n, 수학 : %d점\n, 총점 : %d점\n, 평균: %d점\n",
+         students[i].name, students[i].kor, students[i].eng), students[i].math, students[i].sum, students[i].avg;
+    }
+    fclose(f);
+
+}
+//6.지금까지 구현된 모든 함수를 호출할수 있는 main 함수를 구현
+//  학생정보입력 후 파일저장이 되도록 호출 순서를 결정하시오
+int main(){
+    inputinfomation();
+    generateFile();
+}
